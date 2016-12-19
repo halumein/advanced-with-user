a0fe68d037670a8584a4b6bcc886f9e21da8fd1c
Trishkin Vadim <halumein@gmail.com>
2016-12-19 18:31:30 +0500

    init

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,18 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages', //
+                    'sourceLanguage' => 'ru',
+                    'fileMap' => [
+                        //'main' => 'main.php',
+                    ],
+                ],
+            ],
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
